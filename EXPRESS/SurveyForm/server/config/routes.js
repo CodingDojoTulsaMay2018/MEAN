a1da66569f9ded1d14fe2399400290615ce3c4f4
Frank <frankdepaul23@gmail.com>
2018-08-02 08:23:05 -0500

    Survey Form

--- EXPRESS/SurveyForm/server/config/routes.js
+++ EXPRESS/SurveyForm/server/config/routes.js
@@ -2,8 +2,32 @@ module.exports = app =>{
     console.log("this line 2", this);
     app.get("/", (req, res)=> {
        
-        res.render("index");
+        res.render("index.ejs");
     })
+    app.post("/result", (req, res)=> {
+       
+      
+        
+        req.session.location = req.body.dojoLocation;
+        req.session.name = req.body.fullname;
+        req.session.language = req.body.favoriteLanguage;
+        req.session.comments = req.body.comments;
+        res.redirect("results");
+    })
+
+    app.get("/results", (req, res)=> {
+        
+        var ninja = 
+        {name: req.session.name, 
+        location: req.session.location,
+        language: req.session.language,
+        comments: req.session.comments,
+            
+        } 
+        res.render("results.ejs",{ninja:ninja});
+    })
+
+
    
 
    
