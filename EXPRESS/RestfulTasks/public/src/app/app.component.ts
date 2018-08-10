d48d1ea908d765a57f0674895018673e2ad3355a
Frank <frankdepaul23@gmail.com>
2018-08-09 19:46:03 -0500

    Restful Tasks

--- EXPRESS/RestfulTasks/public/src/app/app.component.ts
+++ EXPRESS/RestfulTasks/public/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular';
   constructor(private _httpService: HttpService){
     
   }
