3f1a038d3ad5cea431b3d44b7cbf62cb5c820380
Frank <frankdepaul23@gmail.com>
2018-08-13 19:39:36 -0500

    .

--- EXPRESS/RestfulUsersNested/public/src/app/user_info/user_info.component.ts
+++ EXPRESS/RestfulUsersNested/public/src/app/user_info/user_info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user_info',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user_info.component.css']
 })
 export class UserInfoComponent implements OnInit {
+  @Input() userToShow: any
 
   constructor() { }
 
   ngOnInit() {
   }
+  
 
 }
